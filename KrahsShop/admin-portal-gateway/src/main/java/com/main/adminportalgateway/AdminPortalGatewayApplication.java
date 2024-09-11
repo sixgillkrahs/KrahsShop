8ac918e66b793c068cc3bc75a406427cf16eb691
quandv <quandv@vdcsoft.vn>
2024-09-11 18:22:31 +0700

    add new gateway

---
+++ KrahsShop/admin-portal-gateway/src/main/java/com/main/adminportalgateway/AdminPortalGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.main.adminportalgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPortalGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminPortalGatewayApplication.class, args);
+    }
+
+}
